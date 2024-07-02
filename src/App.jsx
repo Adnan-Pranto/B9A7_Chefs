b8eba988e8e701b60b855f873d01c2371caf3b0e
Adnan Pranto <prantoshikdar02@gmail.com>
2024-07-03 01:11:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { FaBeer } from "react-icons/fa";
+
+function App() {
+ 
+  return (
+    <>
+      
+      <h1>My Recipe</h1> 
+      <button><FaBeer></FaBeer></button>
+      
+    </>
+  )
+}
+
+export default App
